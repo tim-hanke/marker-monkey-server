0dea570d5f759a14b83fd6743bacf04be6123292
Timothy Hanke <tim.hanke@gmail.com>
2021-01-15 20:37:57 -0500

    change DB_URL to DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://thingful@localhost/thingful",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://thingful@localhost/thingful",
   JWT_SECRET: process.env.JWT_SECRET || "shhh-a-secret",
 };
